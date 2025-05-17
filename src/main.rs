6751de12146ba80b4d7218f7c6f79c03d1188f65
Piotr Puchacz <piotr.puchacz@zoho.com>
2025-05-17 18:13:41 +0200

    Remove comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-// use std::io;
-// use std::io::prelude::*;
 use array2d::{Array2D, Error};
 use std::{io, thread, time};
 use std::io::{Read, Write};
@@ -10,11 +8,8 @@ fn pause() {
     let mut stdin = io::stdin();
     let mut stdout = io::stdout();
 
-    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
-    // write!(stdout, "Enter to continue...").unwrap();
     stdout.flush().unwrap();
 
-    // Read a single byte and discard
     let _ = stdin.read(&mut [0u8]).unwrap();
 }
 
